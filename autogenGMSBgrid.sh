abac0714f2b3b280ea51bd391e0ad77befe2a577
Zhicai <zzhang2@caltech.edu>
2019-07-25 09:56:48 -0700

    add 2018 pythia fragments

--- autogenGMSBgrid.sh
+++ autogenGMSBgrid.sh
@@ -2,14 +2,15 @@
 
 ## config
 year=${1:-"2016"}
-if [[ "${year}" != "2016" ]] && [[ "${year}" != "2017" ]]
+if [[ "${year}" != "2016" ]] && [[ "${year}" != "2017" ]] && [[ "${year}" != "2018" ]]
 then
-    echo "Only 2016 and 2017 are valid options for year! Exiting..."
+    echo "Only 2016, 2017 and 2018 are valid options for year! Exiting..."
     exit
 fi
 
 ## template file
 infile="GMSB_LXXXTeV_CtauYYYcm_Pythia8_13TeV_cff.py"
+infile_2018="GMSB_LXXXTeV_CtauYYYcm_Pythia8_13TeV_cff_2018.py"
 
 ## output directory
 outdir="Configuration/GenProduction/python/ThirteenTeV/GMSB"
@@ -18,25 +19,29 @@ outdir="Configuration/GenProduction/python/ThirteenTeV/GMSB"
 mkdir -p "${outdir}"
 
 ## loop over L, ctau: produce python configs
-for lambda in 100 150 200 250 300 350 400
+for lambda in 100 150 200 250 300 350 400 500
 do
     for ctau in 10 50 100 200 400 600 800 1000 1200 10000
     do
 	outfile="${outdir}/GMSB_L${lambda}TeV_Ctau${ctau}cm_Pythia8_13TeV_cff.py"
-
-	sed "s/XXX/${lambda}/" <"${infile}" > "${outfile}"
-	sed -i '' "s/YYY/${ctau}/" "${outfile}"
-
 	if [[ "${year}" == "2016" ]]
 	then
+	    sed "s/XXX/${lambda}/" <"${infile}" > "${outfile}"
+	    sed -i '' "s/YYY/${ctau}/" "${outfile}"
 	    sed -i '' "s/FILE/Pythia8CUEP8M1/" "${outfile}"
 	    sed -i '' "s/TUNE/CUEP8M1/" "${outfile}"
 	elif [[ "${year}" == "2017" ]]
 	then
+	    sed "s/XXX/${lambda}/" <"${infile}" > "${outfile}"
+	    sed -i '' "s/YYY/${ctau}/" "${outfile}"
 	    sed -i '' "s/FILE/MCTunes2017.PythiaCP5/" "${outfile}"
 	    sed -i '' "s/TUNE/CP5/" "${outfile}"
+	elif [[ "${year}" == "2018" ]]
+	then
+	    sed "s/XXX/${lambda}/" <"${infile_2018}" > "${outfile}"
+	    sed -i "s/YYY/${ctau}/" "${outfile}"
 	else
-	    echo "How did this happen?? Only 2016 and 2017 are valid years! Exiting..."
+	    echo "How did this happen?? Only 2016, 2017 and 2018  are valid years! Exiting..."
 	    exit
 	fi
 
