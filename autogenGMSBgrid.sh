838e6aac1d13251e050c0ee8c4ed26ca0c6cef7e
Zhicai <zzhang2@caltech.edu>
2019-09-24 13:05:33 -0700

    put SLHA tables in python fragment directly

--- autogenGMSBgrid.sh
+++ autogenGMSBgrid.sh
@@ -17,13 +17,14 @@ outdir="Configuration/GenProduction/python/ThirteenTeV/GMSB"
 
 ## make directory
 mkdir -p "${outdir}"
+mkdir -p "${outdir}_noSLHA"
 
 ## loop over L, ctau: produce python configs
 for lambda in 100 150 200 250 300 350 400 450 500
 do
     for ctau in 10 50 100 200 400 600 800 1000 1200 10000
     do
-	outfile="${outdir}/GMSB_L${lambda}TeV_Ctau${ctau}cm_Pythia8_13TeV_cff.py"
+	outfile="${outdir}_noSLHA/GMSB_L${lambda}TeV_Ctau${ctau}cm_Pythia8_13TeV_cff.py"
 	if [[ "${year}" == "2016" ]]
 	then
 	    sed "s/XXX/${lambda}/" <"${infile}" > "${outfile}"
@@ -44,6 +45,16 @@ do
 	    echo "How did this happen?? Only 2016, 2017 and 2018  are valid years! Exiting..."
 	    exit
 	fi
-
+	outfile_withSLHA="${outdir}/GMSB_L${lambda}TeV_Ctau${ctau}cm_Pythia8_13TeV_cff.py"
+	echo "" > ${outfile_withSLHA}
+	echo "SLHA_TABLE = '''" >> ${outfile_withSLHA}
+	cat Configuration/Generator/data/GMSB/GMSB_Lambda${lambda}TeV_CTau${ctau}cm.slha >> ${outfile_withSLHA}
+	echo "'''" >> ${outfile_withSLHA}
+	echo "" >> ${outfile_withSLHA}
+	cp ${outfile} temp.txt
+	sed -i "/with open/d" temp.txt
+	sed -i "/read()/d" temp.txt
+	cat temp.txt >> ${outfile_withSLHA}
+	rm temp.txt
     done
 done
